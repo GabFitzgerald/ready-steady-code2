0a3f28a0a47b0ce71c37155fc50b3d5aadaed3fb
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-05-07 08:48:24 +1000

    recipes_ingredient join table created

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
     has_many :users_ingredients
     has_many :users, through: :users_ingredients
-
+    has_many :recipes_ingredients
+    has_many :recipes, through: :recipes_ingredients
 end
